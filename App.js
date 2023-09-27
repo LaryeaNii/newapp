57f6c913cbd5fcd39f0395d46995778c9f88ae66
LaryeaNii <zadoklaryea@gmail.com>
2023-09-27 10:23:45 +0000

    first ever mobile app

--- App.js
+++ App.js
@@ -1,11 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [listItems, setListItems] = useState([
+    {
+      key: 1,
+      task: "Finish novel",
+    },
+    {
+      key: 2,
+      task: "Finish assignment",
+    },
+    {
+      key: 3,
+      task: "Finish project",
+    },
+    {
+      key: 4,
+      task: "Finish homework",
+    },
+  ]);
+
+  const [taskText, setTaskText] = useState("");
+
+  const addTask = () => {
+    if (taskText) {
+      const newTask = {
+        key: listItems.length + 1,
+        task: taskText,
+      };
+
+      const newList = [...listItems, newTask];
+      setListItems(newList);
+      setTaskText("");
+    }
+  };
+
+  const deleteTask = (key) => {
+    const newList = listItems.filter((item) => item.key !== key);
+    setListItems(newList);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>My Grocery List</Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a task"
+          value={taskText}
+          onChangeText={setTaskText}
+        />
+        <View style={styles.button}>
+          <Button title="Add Task" color="black" onPress={addTask} />
+        </View>
+      </View>
+      <Text style={{ textAlign: "center" }}>
+        You have <Text style={{ fontWeight: "bold" }}>{listItems.length}</Text>{" "}
+        item(s) remaining on your list
+      </Text>
+      {listItems.length > 0 ? (
+        <View style={styles.listItems}>
+          <FlatList
+            data={listItems}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => deleteTask(item.key)}>
+                <Text style={styles.taskText}>
+                  {item.key}. {item.task}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      ) : (
+        <Text style={styles.noText}>New Items Appear Here</Text>
+      )}
     </View>
   );
 }
@@ -13,8 +93,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+  },
+  header: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 80,
+    elevation: 2,
+    zIndex: 1,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    width: 300,
+    textAlign: "center",
+    zIndex: 2,
+    elevation: 2,
+  },
+  listItems: {
+    flex: 4,
+    padding: 10,
+    alignItems: "center",
+  },
+  input: {
+    margin: 20,
+    borderBottomWidth: 1,
+    borderColor: "grey",
+    padding: 10,
+    marginBottom: 10,
+  },
+  taskText: {
+    fontSize: 20,
+    padding: 10,
+  },
+  button: {
+    padding: 20,
+    margin: 20,
+  },
+  noText: {
+    flex: 3,
+    textAlign: "center",
+    fontSize: 14,
+    paddingTop: 10,
   },
 });
